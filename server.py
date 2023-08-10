558ced0de7478e3cdda0527233e04b7e9e1becdd
Glitch (glitch-hello-website) <none>
2023-08-10 17:10:02 +0000

    🏜🍱 Checkpoint ./start.sh:60561907/20 ./requirements.txt:60561907/29 ./server.py:60561907/1128 ./templates/index.html:60561907/15667 ./templates/predict.html:60561907/8282 ./templates/contact.html:60561907/19267

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import *
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def default():
+    return render_template('index.html')
+  
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+  
+@app.route('/p', methods=["POST"])
+def p():
+  brand = request.form.get('brand')
+  fiveg = request.form.get('5g')
+  rom = request.form.get('rom')
+  ram = request.form.get('ram')
+  days = request.form['days']
+      
+  
+  url = 'https://raw.githubusercontent.com/mannikdeep/usedmobile/main/main_price.csv'
+  main = pd.read_csv(url)
+  x = main.loc[:,'device_brand':'days_used']
+  y = main['price']
+  model = LinearRegression()
+  model.fit(x,y)
+  
+  try:
+    res = model.predict([[eval(brand), eval(fiveg), eval(rom), eval(ram), eval(days)]])
+    res = "Rs. "+ str(round(res[0],2))
+    return render_template('predict.html', data = res)
+  except:
+    return render_template('predict.html', data = "Please Enter Valid Values")
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+  
+
+if __name__ == '__main__':
+  app.run(debug=True)
